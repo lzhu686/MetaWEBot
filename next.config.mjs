3a289aa90de6824ba089575418580dbabc746ac4
lzhu686 <lzhu686@connect.hkust-gz.edu.cn>
2024-10-31 14:59:06 +0800

    add camera

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['cdn.jsdelivr.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        pathname: '**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'mind-ar': 'MINDAR',
+      'aframe': 'AFRAME',
+    })
+    return config
+  },
+};
 
 export default nextConfig;
---
+++ src/types/aframe.d.ts
@@ -0,0 +1,10 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      'a-scene': any;
+      'a-entity': any;
+      'a-camera': any;
+      'a-assets': any;
+      'a-asset-item': any;
+      'a-gltf-model': any;
+    }
+  }
